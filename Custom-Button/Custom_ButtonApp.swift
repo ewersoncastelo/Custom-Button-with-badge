d5ed90e7b20a9466e6ccac28855351dd1877f9ed
Ewerson Castelo <ewerson.castelo@gmail.com>
2021-10-30 12:56:11 -0400

    Initial Commit

---
+++ Custom-Button/Custom_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ButtonApp.swift
+//  Custom-Button
+//
+//  Created by ewerson on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
